1202c1b21c224fff505bdd7088da497f1631bb17
AnnaMartsinovska <annmartsinovska@gmail.com>
2023-09-22 05:10:30 +0300

    task 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,74 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const refs = {
+  ulGallery: document.querySelector('.gallery'),
+};
+
+function galleryTemplate({preview, original, description}) {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+};
+ 
+
+function renderGallery() {
+    const markup = galleryItems.
+        map(galleryTemplate).
+        join('');
+    refs.ulGallery.innerHTML = markup;
+};
+ 
+renderGallery();
+
+refs.ulGallery.addEventListener('click', e => {
+    e.preventDefault();
+
+    if (e.target.nodeName !== 'IMG') return;
+
+    showModal(e);
+});
+
+let modalGallery = null;
+   
+
+function showModal(e) { 
+    modalGallery = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" alt="${e.target.alt}">
+`,
+     {
+    onShow: () => {
+            document.addEventListener('keydown', closeEskModal);
+            document.body.style.overflow = 'hidden';
+        },
+    onClose: () => {
+           document.removeEventListener('keydown', closeEskModal);
+    document.body.style.overflow = 'visible';
+       }
+    }    
+);
+
+    modalGallery.show();
+};
+
+
+function closeEskModal(e) {
+    if (e.code !== 'Escape') return;
+
+    closeModal()
+};
+ 
+
+function closeModal() {
+    
+    modalGallery.close();
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  ulGallery: document.querySelector('.gallery'),
+};
+
+function galleryTemplate({preview, original, description}) {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+};
+ 
+
+function renderGallery() {
+    const markup = galleryItems.
+        map(galleryTemplate).
+        join('');
+    refs.ulGallery.innerHTML = markup;
+    
+};
+ 
+renderGallery();
+
+
+refs.ulGallery.addEventListener('click', e =>
+    e.preventDefault());
+
+    
+
+new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+});
